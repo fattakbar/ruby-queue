87ef8902ff19b78634c7b36b98b6ae1e78fdd2b9
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-04-01 15:04:38 +0700

    antrian_bank.rb Done

--- antrian_bank.rb
+++ antrian_bank.rb
@@ -1,5 +1,7 @@
 require_relative 'queue.rb'
 
+q = MyQueue.new(3)
+
 while true do
     puts '#'*30
     puts '1. Daftar Antrian'
@@ -11,8 +13,26 @@ while true do
 
     case input
     when 1
+        begin
+            print 'Nama Pasien : '
+            name = gets.chomp
+            q.enqueue(name)
+        rescue MyQueue::QueueFull
+            puts 'Antrian Penuh'
+        end
     when 2
+        begin
+            name = q.dequeue
+            puts "Pasien Dengan Nama #{name} Harap Menuju Ke Meja Teller"
+        rescue MyQueue::QueueEmpty
+            puts 'Antrian Kosong'
+        end
     when 3
+        puts 'Daftar Antrian :'
+        queue = q.queue
+        queue.each do |p|
+            puts p
+        end
     else
         puts 'Menu Yang Anda Pilih Tidak Tersedia'
     end
